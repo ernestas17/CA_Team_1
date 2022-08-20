e277ffe2d173e23ced1ddc7276285f003dde786c
Domenika Grinkeviciute Skorulske <d.grinkeviciute@gmail.com>
2022-08-20 22:24:51 +0300

    Created a responsive "Contacts page"

--- src/pages/ContactPage/ContactPage.jsx
+++ src/pages/ContactPage/ContactPage.jsx
@@ -1,7 +1,85 @@
-import React from "react";
+import { Wrapper, Status } from "@googlemaps/react-wrapper";
+
+import Button from "../../components/Button";
+import Map from "./components/Map";
+
+import {
+  StyledSectionPrimary,
+  StyledSectionWrapper,
+  StyledMapSection,
+} from "./ContactPage.style";
+import contactUsBackgroundImage from "./photos/contactUsBackground.png";
 
 const ContactPage = () => {
-  return <div>ContactPage</div>;
+  return (
+    <main>
+      <StyledSectionPrimary>
+        <StyledSectionWrapper>
+          <h2>Get in touch with our experts</h2>
+          <div className="layout">
+            <div className="form">
+              <div className="inputs">
+                <input type="text" placeholder="Your full Name"></input>
+                <input type="text" placeholder="Your Email"></input>
+                <input type="text" placeholder="Select service type"></input>
+                <input type="text" placeholder="Message"></input>
+              </div>
+
+              <Button type="primary" buttontext="Send message" />
+            </div>
+            <div className="moreInformation">
+              <div>
+                <p>Address</p>
+                <h6>NH 234 Public Square San Francisco 65368</h6>
+              </div>
+              <div>
+                <p>Contact Details</p>
+                <h6>
+                  1800 265 24 52 <br />
+                  Finsweet@gmail.com
+                </h6>
+              </div>
+              <div>
+                <p>Opening Hours</p>
+                <h6>Monday to Friday 9:00 AM to 10:00 AM</h6>
+              </div>
+            </div>
+          </div>
+        </StyledSectionWrapper>
+      </StyledSectionPrimary>
+
+      <StyledSectionPrimary>
+        <StyledMapSection>
+          <div
+            className="innerLayout"
+            // style={{
+            //   backgroundImage: `url(${contactUsBackgroundImage})`,
+            //   backgroundRepeat: "no-repeat",
+            //   backgroundPosition: "center",
+            //   backgroundSize: "cover",
+            //   width: "fit-content",
+            //   height: "792px",
+            // }}
+          >
+            <div className="mapContainer">
+              <Map />
+            </div>
+            <div
+              className="backgroundImage"
+              style={{
+                backgroundImage: `url(${contactUsBackgroundImage})`,
+                backgroundRepeat: "no-repeat",
+                backgroundPosition: "center 100%",
+                backgroundSize: "cover",
+                width: "100%",
+                height: "448px",
+              }}
+            ></div>
+          </div>
+        </StyledMapSection>
+      </StyledSectionPrimary>
+    </main>
+  );
 };
 
 export default ContactPage;
---
+++ src/pages/ContactPage/ContactPage.style.jsx
@@ -0,0 +1,161 @@
+import styled from "styled-components";
+
+const device = {
+  mobile: `(max-width: 628px)`,
+  tablet: `(max-width: 1280px)`,
+};
+
+export const StyledSectionPrimary = styled.div`
+  width: 100%;
+`;
+
+export const StyledSectionWrapper = styled.div`
+  max-width: 1280px;
+  margin: 0 auto;
+
+  display: flex;
+  flex-direction: column;
+  gap: 48px;
+
+  padding: 80px 0 128px 0;
+
+  h2 {
+    width: 38%;
+
+    color: #1e1b1b;
+
+    font-weight: 800;
+    font-size: 64px;
+    line-height: 84px;
+    letter-spacing: -0.03em;
+  }
+
+  .layout {
+    display: flex;
+    justify-content: space-between;
+    gap: 24px;
+
+    .form {
+      width: 50%;
+
+      display: flex;
+      flex-direction: column;
+      gap: 48px;
+
+      .inputs {
+        width: 100%;
+
+        display: flex;
+        flex-direction: column;
+        gap: 4px;
+
+        input {
+          width: 100%;
+          background: #f2f2f2;
+
+          border: 0;
+          border-radius: 2px;
+
+          padding: 23px 40px;
+
+          ::placeholder {
+            color: #1e1b1b;
+
+            font-weight: 600;
+            font-size: 16px;
+            line-height: 28px;
+          }
+        }
+      }
+    }
+
+    .moreInformation {
+      width: 40%;
+
+      display: flex;
+      flex-direction: column;
+      gap: 32px;
+
+      p {
+        color: #1e1b1b;
+        opacity: 0.7;
+
+        font-weight: 600;
+        font-size: 16px;
+        line-height: 28px;
+      }
+
+      h6 {
+        color: #1e1b1b;
+
+        font-weight: 600;
+        font-size: 24px;
+        line-height: 36px;
+        letter-spacing: -0.03em;
+      }
+    }
+  }
+
+  @media ${device.tablet} {
+    padding: 80px 24px 128px 24px;
+
+    h2 {
+      width: 100%;
+    }
+  }
+
+  @media ${device.mobile} {
+    h2 {
+      text-align: center;
+    }
+
+    .layout {
+      flex-direction: column;
+      gap: 96px;
+
+      .form {
+        width: 100%;
+
+        button {
+          width: 100%;
+        }
+      }
+
+      .moreInformation {
+        width: 100%;
+        text-align: center;
+      }
+    }
+  }
+`;
+
+export const StyledMapSection = styled.div`
+  width: 100%;
+  margin: 0 auto;
+
+  .innerLayout {
+    width: 100%;
+    height: 792px;
+
+    position: relative;
+
+    .backgroundImage {
+      position: absolute;
+      bottom: 0;
+      z-index: -1;
+    }
+
+    .mapContainer {
+      max-width: 1280px;
+      margin: 0 auto;
+    }
+  }
+
+  @media ${device.tablet} {
+    .innerLayout {
+      .mapContainer {
+        margin: 0 24px;
+      }
+    }
+  }
+`;
