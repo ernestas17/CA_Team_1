57a6ddc69c8331366e2571c3770cec8caa95a3c8
Augustinas Mazutis <chapropax@gmail.com>
2022-08-21 00:55:41 +0300

    Created reusable Card

---
+++ src/pages/BlogPage/components/card/Card.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import { StyledCard } from './Card.style';
+import arrow from '../../photos/Arrow.png';
+const Card = ({ image, posteddata, hg, text, url }) => {
+  return (
+    <StyledCard>
+      <div className='container'>
+        <div
+          className='image'
+          style={{ backgroundImage: `url(${image})` }}
+        ></div>
+        <div className='posted-data'>{posteddata}</div>
+        <h4>{hg}</h4>
+        <p>{text}</p>
+        <div className='arrow'>
+          <div>Read more</div>
+          <div>
+            <a href={`/blog/${url}`}>
+              <img src={arrow} alt='' />
+            </a>
+          </div>
+        </div>
+      </div>
+    </StyledCard>
+  );
+};
+
+export default Card;
