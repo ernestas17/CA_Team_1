40c037f74942eb6bfe5ad52bb2cb985d46811fa0
Augustinas Mazutis <chapropax@gmail.com>
2022-08-22 12:51:13 +0300

    Created shared text file, for pages

---
+++ src/shared/texts.js
@@ -0,0 +1,89 @@
+const TEXTS = {
+  //Blog Page
+  //   ShouldIBuyaNewCarorLeaseaNewCarin2021
+  SingleBlogPage: {
+    ShouldIBuyaNewCarorLeaseaNewCarin2021: {
+      url: 'ShouldIBuyaNewCarorLeaseaNewCarin2021',
+      img: 'ShouldIBuyaNewCarorLeaseaNewCarin2021',
+      posteddate: 'Posted on October 6th 2021',
+      title: 'Should I Buy a New Car or Lease a New Car in 2021?',
+      underTitleText:
+        'We provide a full range of front end mechanical repairs for all makes and models of cars, no matter the cause. This includes, We provide a full range of front end mechanical ',
+    },
+    GetanAwesomeNumberPlateforYourNewCar: {
+      url: 'GetanAwesomeNumberPlateforYourNewCar',
+      img: 'americanvehiclenumberplates',
+      posteddate: 'Posted on October 6th 2021',
+      title: 'Get an Awesome Number Plate for Your New Car',
+      underTitleText:
+        'We provide a full range of front end mechanical repairs for all makes and models of cars, no matter the cause. This includes, We provide a full range of front end mechanical ',
+    },
+    WouldyouLetaRobotDriveyourCarOurAIFuture: {
+      url: 'WouldyouLetaRobotDriveyourCarOurAIFuture',
+      img: 'WouldyouLetaRobotDriveyourCarOurAIFuture',
+      posteddate: 'Posted on October 6th 2021',
+      title: 'Would you Let a Robot Drive your Car? Our AI Future',
+      underTitleText:
+        'We provide a full range of front end mechanical repairs for all makes and models of cars, no matter the cause. This includes, We provide a full range of front end mechanical ',
+    },
+    WillElectricTechnologySoonRuleTheRoad: {
+      url: 'WillElectricTechnologySoonRuleTheRoad',
+      img: 'WillElectricTechnologySoonRuleTheRoad',
+      posteddate: 'Posted on October 6th 2021',
+      title: 'Would you Let a Robot Drive your Car? Our AI Future',
+      underTitleText:
+        'We provide a full range of front end mechanical repairs for all makes and models of cars, no matter the cause. This includes, We provide a full range of front end mechanical ',
+    },
+    // CardM
+    geniuscaraccessoriesyoushouldneverdrivewithout: {
+      url: '5-genius-car-accessories-you-should-never-drive-without',
+      img: 'WillElectricTechnologySoonRuleTheRoad',
+      posteddate: 'Posted on October 6th 2021',
+      title: 'Would you Let a Robot Drive your Car? Our AI Future',
+      underTitleText:
+        'We provide a full range of front end mechanical repairs for all makes and models of cars, no matter the cause. This includes, We provide a full range of front end mechanical ',
+    },
+    geniuscaraccessoriesyoushouldneverdrivewithoutsecond: {
+      url: '5-genius-car-accessories-you-should-never-drive-without-second',
+      img: 'ShouldIBuyaNewCarorLeaseaNewCarin2021',
+      posteddate: 'Posted on October 6th 2021',
+      title: '5 Genius Car Accessories You Should Never Drive Without',
+      underTitleText:
+        'We provide a full range of front end mechanical repairs for all makes and models of cars, no matter the cause. This includes, We provide a full range of front end mechanical ',
+    },
+    weprovideblowstraightpastitwiththewheelsskidding: {
+      url: 'we-provide-blow-straight-past-it-with-the-wheels-skidding',
+      img: 'WouldyouLetaRobotDriveyourCarOurAIFuture',
+      posteddate: 'Posted on October 6th 2021',
+      title: 'We provide blow straight past it with the wheels skidding',
+      underTitleText:
+        'We provide a full range of front end mechanical repairs for all makes and models of cars, no matter the cause. This includes, We provide a full range of front end mechanical ',
+    },
+    weprovideblowstraightpastitwiththewheelsskiddingsecond: {
+      url: 'we-provide-blow-straight-past-it-with-the-wheels-skidding-second',
+      img: 'WouldyouLetaRobotDriveyourCarOurAIFuture',
+      posteddate: 'Posted on October 6th 2021',
+      title: 'We provide blow straight past it with the wheels skidding',
+      underTitleText:
+        'We provide a full range of front end mechanical repairs for all makes and models of cars, no matter the cause. This includes, We provide a full range of front end mechanical ',
+    },
+    geniuscaraccessoriesyoushouldneverdrivewithoutthird: {
+      url: '5-genius-car-accessories-you-should-never-drive-without-third',
+      img: 'ShouldIBuyaNewCarorLeaseaNewCarin2021',
+      posteddate: 'Posted on October 6th 2021',
+      title: '5 Genius Car Accessories You Should Never Drive Without',
+      underTitleText:
+        'We provide a full range of front end mechanical repairs for all makes and models of cars, no matter the cause. This includes, We provide a full range of front end mechanical ',
+    },
+    geniuscaraccessoriesyoushouldneverdrivewithoutfourth: {
+      url: '5-genius-car-accessories-you-should-never-drive-without-fourth',
+      img: 'ShouldIBuyaNewCarorLeaseaNewCarin2021',
+      posteddate: 'Posted on October 6th 2021',
+      title: '5 Genius Car Accessories You Should Never Drive Without',
+      underTitleText:
+        'We provide a full range of front end mechanical repairs for all makes and models of cars, no matter the cause. This includes, We provide a full range of front end mechanical ',
+    },
+  },
+};
+
+export default TEXTS;
